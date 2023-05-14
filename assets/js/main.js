128b74e48a909e74e1deecb362d249041fbddcf3
freetimezp <kraftukrnet@ukr.net>
2023-05-14 22:16:02 +0300

    04. js-social-10-3d-gallery. Show floor.

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,8 +34,25 @@ let geometry = new THREE.BoxGeometry(1, 1, 1); //box geometry is the shape of ob
 //material
 let material = new THREE.MeshBasicMaterial({ color: "#7282ca" }); // color of the object
 let cube = new THREE.Mesh(geometry, material); // create cube
-
 scene.add(cube);
+
+//texture of the floor
+let floorTexture = new THREE.TextureLoader().load('assets/img/floor.jpg');
+
+floorTexture.wrapS = THREE.RepeatWrapping; // horisontal
+floorTexture.wrapT = THREE.RepeatWrapping; // vertical
+floorTexture.repeat.set(20, 20); // how many times texture repeat
+
+//create floor plane
+let planeGeometry = new THREE.PlaneGeometry(50, 50); //box geometry is the shape of object
+let planeMaterial = new THREE.MeshBasicMaterial({
+    map: floorTexture,
+    side: THREE.DoubleSide
+}); // color of the object
+let floorPlane = new THREE.Mesh(planeGeometry, planeMaterial); // create floor
+floorPlane.rotation.x = Math.PI / 2; // this is 90 deg
+floorPlane.position.y = -Math.PI; // this is -180 deg
+scene.add(floorPlane);
 
 //add controls, when we press keys
 document.addEventListener('keydown', onKeyDown, false);
