edeaa250e2d40b4977a7101db201e3151dd78ac9
freetimezp <kraftukrnet@ukr.net>
2023-05-16 21:48:08 +0300

    14. js-social-10-3d-gallery. Refactor js. Add images, data to gallery. Show info if close to picture.

--- client/src/assets/js/main.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-
-import BgMenu from '../../../components/BgMenu';
-
-const root = createRoot(document.getElementById('root'));
-root.render(<BgMenu />);
